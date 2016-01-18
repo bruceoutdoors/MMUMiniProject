e11aef2a0fcc178892c9543f73526b272c2178f8
Lee Zhen Yong <bruceoutdoors@gmail.com>
2016-01-18 21:56:55 +0800

    Have Faculty implements Serializable

--- src/main/java/app/model/Faculty.java
+++ src/main/java/app/model/Faculty.java
@@ -32,14 +32,15 @@ import javax.xml.bind.annotation.XmlTransient;
     @NamedQuery(name = "Faculty.findAll", query = "SELECT f FROM Faculty f"),
     @NamedQuery(name = "Faculty.findByFacId", query = "SELECT f FROM Faculty f WHERE f.facId = :facId"),
     @NamedQuery(name = "Faculty.findByFacName", query = "SELECT f FROM Faculty f WHERE f.facName = :facName")})
-public class Faculty {
+public class Faculty implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 10)
     @Column(name = "fac_id")
-    private int facId;
+    private Integer facId;
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 40)
@@ -51,20 +52,20 @@ public class Faculty {
     public Faculty() {
     }
 
-    public Faculty(int facId) {
+    public Faculty(Integer facId) {
         this.facId = facId;
     }
 
-    public Faculty(int facId, String facName) {
+    public Faculty(Integer facId, String facName) {
         this.facId = facId;
         this.facName = facName;
     }
 
-    public int getFacId() {
+    public Integer getFacId() {
         return facId;
     }
 
-    public void setFacId(int facId) {
+    public void setFacId(Integer facId) {
         this.facId = facId;
     }
 
@@ -84,4 +85,30 @@ public class Faculty {
     public void setStudentCollection(Collection<Student> studentCollection) {
         this.studentCollection = studentCollection;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (facId != null ? facId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Faculty)) {
+            return false;
+        }
+        Faculty other = (Faculty) object;
+        if ((this.facId == null && other.facId != null) || (this.facId != null && !this.facId.equals(other.facId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "app.model.Faculty[ facId=" + facId + " ]";
+    }
+    
 }
