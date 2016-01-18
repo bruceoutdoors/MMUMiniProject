8d49077bdd5dff66ba5f82b26c9f111e84db3222
Lee Zhen Yong <bruceoutdoors@gmail.com>
2016-01-18 13:24:21 +0800

    [amirah] Add Faculties and Specializations

---
+++ src/main/resources/db/migration/V3__Add_Faculties_Specializations.sql
@@ -0,0 +1,24 @@
+/**
+ * Author:  Amirah
+ * Created: Jan 18, 2016
+ */
+
+Insert into faculty (fac_id, fac_name) values 
+(100, 'Faculty of Computer Science'),
+(101, 'Faculty of Management'),
+(102, 'Faculty of Engineering'),
+(103, 'Faculty of Creative Multimedia');
+
+Insert into specialization (spec_id, spec_name) values 
+(200, 'Software Engineering'),
+(201, 'Information System'),
+(202, 'Games Development'),
+(203, 'Management'),
+(204, 'Economics'),
+(205, 'Finance'),
+(206, 'Electrical'),
+(207, 'Electronics'),
+(208, 'Mechanical'),
+(209, 'Multimedia Graphics'),
+(210, 'Animation'),
+(211, 'Multimedia Games');
