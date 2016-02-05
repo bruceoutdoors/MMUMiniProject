4d3f86ece07348106ff74258fef21e21210686ad
Lee Zhen Yong <bruceoutdoors@gmail.com>
2016-02-05 16:02:12 +0800

    default user is bruce. password is bruce

---
+++ src/main/resources/db/migration/V19__TEMP_Add_Default_Admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user` 
+(`user_id`, `user_name`, `user_email`, `user_tel`, `user_lastSignIn`, `role_id`, `user_active`, `user_password`) 
+VALUES ('42', 'bruce', 'bruce@bruce.com', '123456', NULL, '0', '1', 'bruce');
+INSERT INTO `admin`  
+VALUES ('42');
